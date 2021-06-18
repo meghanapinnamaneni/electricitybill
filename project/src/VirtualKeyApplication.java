0945332e70e076bdb3b22f7505b8fc20ea290d8e
hp <hp@MEGHANA>
2021-06-18 12:26:38 +0530

    project

---
+++ project/src/VirtualKeyApplication.java
@@ -0,0 +1,12 @@
+import screens.Welcome;
+
+public class VirtualKeyApplication {
+
+    public static void main(String[] args) {
+    	
+    	Welcome welcome = new Welcome();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
---
+++ project/src/services/Screen.java
@@ -0,0 +1,12 @@
+package services;
+public interface Screen {
+
+    public void Show1();
+
+    public void NavigateOption(int option);
+    
+    public void GetUserInput();
+
+	public void Show();
+
+}
